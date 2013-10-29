4462e5ac2b1e04af11e22e29692bb600c954cbaf
Brian Cardarella <bcardarella@gmail.com>
2013-10-29 14:25:58 -0400

    Cleaned up some files

--- test/dummy/app/assets/javascripts/ember-app.js
+++ test/dummy/app/assets/javascripts/ember-app.js
@@ -4,9 +4,6 @@
 //= require_tree ./models
 //= require_tree ./templates
 //= require_tree ./controllers
-//= require_tree ./components
-//= require_tree ./adapters
-//= require_tree ./utils
 //= require_tree ./routes
 
 window.App = require('app').default.create();
