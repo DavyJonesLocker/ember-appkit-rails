70f0243cc286c2504e2ef49465beadc35dfd61ae
Brian Cardarella <bcardarella@gmail.com>
2013-10-29 18:27:09 -0400

    Strange thing with Rails engines in the test env
    jquery-rails requires an explicit require in application.rb
    
    See: https://www.ruby-forum.com/topic/2484569#1021529
    
    Closes #2

--- test/integration/navigation_test.rb
+++ test/integration/navigation_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class NavigationTest < ActionDispatch::IntegrationTest
   test 'can render home page' do
-    assert page.has_content 'Brian Cardarella'
+    visit root_path
+    assert page.has_text? 'Joe Doe'
   end
 end
 
