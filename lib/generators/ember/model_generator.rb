27e5e6a3d902a03f209a16440cfceb2cd56964b2
Alex Navasardyan <alex.navasardyan@yahoo.com>
2013-10-30 16:39:41 -0400

    adding model generator

---
+++ lib/generators/ember/model_generator.rb
@@ -0,0 +1,47 @@
+require 'generators/ember/generator_helpers'
+
+module Ember
+  module Generators
+    class ModelGenerator < ::Rails::Generators::NamedBase
+      include Ember::Generators::GeneratorHelpers
+
+      source_root File.expand_path("../../templates", __FILE__)
+      desc "Creates a new ember.js model"
+      argument :attributes, :type => :array, :default => [], :banner => "field[:type] field[:type] ..."
+
+      class_option :ember_path, :type => :string, :aliases => "-d", :default => false, :desc => "Custom ember app path"
+
+      def create_model_files
+        file_path = File.join(ember_path, 'models', class_path, "#{file_name}.js.es6")
+        template "model.js.es6", file_path
+      end
+
+    private
+      EMBER_TYPE_LOOKUP = {
+        nil  => 'string',
+        :binary => 'string',
+        :string => 'string',
+        :text => 'string',
+        :boolean => 'boolean',
+        :date => 'date',
+        :datetime =>'date',
+        :time => 'date',
+        :timestamp => 'date',
+        :decimal => 'number',
+        :float => 'number',
+        :integer => 'number',
+        :primary_key => 'number'
+      }
+
+      def parse_attributes!
+        self.attributes = (attributes || []).map do |attr|
+          name, type = attr.split(':')
+          key = type.try(:to_sym)
+          ember_type = EMBER_TYPE_LOOKUP[key] || type
+
+          { :name => name, :type => ember_type }
+        end
+      end
+    end
+  end
+end
---
+++ test/generators/model_generator_test.rb
@@ -0,0 +1,48 @@
+require 'test_helper'
+require 'generators/ember/model_generator'
+
+class ModelGeneratorTest < Rails::Generators::TestCase
+  include GeneratorTestSupport
+
+  tests Ember::Generators::ModelGenerator
+  destination File.join(Rails.root, "tmp", "generator_test_output")
+
+  setup :prepare_destination
+
+  test "create model" do
+    run_generator ["post", "title:string"]
+    assert_file "app/assets/javascripts/models/post.js.es6"
+  end
+
+  test "create namespaced model" do
+    run_generator ["post/doineedthis", "title:string"]
+    assert_file "app/assets/javascripts/models/post/doineedthis.js.es6"
+  end
+
+  test "leave parentheses when create model w/o attributes" do
+    run_generator ["post"]
+    assert_file "app/assets/javascripts/models/post.js.es6", /export default = DS.Model.extend/
+  end
+
+  test "Assert files are properly created" do
+    run_generator %w(ember)
+
+    assert_file "#{ember_path}/models/ember.js.es6"
+  end
+
+  test "Assert files are properly created with custom path" do
+    custom_path = ember_path("custom")
+    run_generator [ "ember", "-d", custom_path ]
+
+    assert_file "#{custom_path}/models/ember.js.es6"
+  end
+
+  test "Uses config.ember.ember_path" do
+    custom_path = ember_path("custom")
+
+    with_config ember_path: custom_path do
+      run_generator ["ember"]
+      assert_file "#{custom_path}/models/ember.js.es6"
+    end
+  end
+end
