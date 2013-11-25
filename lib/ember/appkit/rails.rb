29b6c74735190a74af1c2e513239960955b6b02e
Brian Cardarella <bcardarella@gmail.com>
2013-11-25 03:57:36 -0500

    Correct load order

--- lib/ember/appkit/rails.rb
+++ lib/ember/appkit/rails.rb
@@ -3,9 +3,9 @@ module Ember
 end
 
 require 'ember-rails'
-require 'ember/appkit/rails/resource_override'
 require 'ember/source'
 require 'ember/appkit/rails/engine'
+require 'ember/appkit/rails/resource_override'
 require 'es6_module_transpiler/rails'
 
 Rails::WelcomeController.view_paths = File.expand_path('../rails/templates', __FILE__)
