45039d97775554cbdc6c2f817e7dc033cab0a3e5
Brian Cardarella <bcardarella@gmail.com>
2013-11-30 10:13:29 -0500

    Adding view path no longer necessary

--- lib/ember/appkit/rails.rb
+++ lib/ember/appkit/rails.rb
@@ -2,5 +2,3 @@ module Ember::Appkit::Rails; end
 
 require 'ember/appkit/rails/engine'
 require 'ember/appkit/rails/sprockets'
-
-Rails::WelcomeController.view_paths = File.expand_path('../rails/templates', __FILE__)
