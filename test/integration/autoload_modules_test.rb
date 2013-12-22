1891e133bdcd3d06c3f75bccd5a1e356b48c90d1
Brian Cardarella <bcardarella@gmail.com>
2013-12-22 04:22:08 -0500

    Ensure directories and files exist for autoload tests

--- test/integration/autoload_modules_test.rb
+++ test/integration/autoload_modules_test.rb
@@ -4,12 +4,17 @@ class AutoloadModulesTest < ActionDispatch::IntegrationTest
   include IntegrationTestSupport
 
   test 'does not auto create modules for directories that do not have ruby files' do
+    FileUtils.mkdir(File.join(Rails.root, 'app/models/not_a_module'))
+
     assert_raise NameError do
       NotAModule
     end
   end
 
   test 'does create modules for directories that have ruby files' do
+    FileUtils.mkdir(File.join(Rails.root, 'app/models/is_a_module'))
+    FileUtils.touch(File.join(Rails.root, 'app/models/is_a_module/dog.rb'))
+
     assert_nothing_raised NameError do
       IsAModule
     end
