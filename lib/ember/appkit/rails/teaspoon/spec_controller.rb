39738f3a19b08f0a45d22216ec353aacb7bd5aff
Aaron Renoir <aaron@fieldphone.com>
2014-03-14 14:40:14 -0700

    remove whitespace

--- lib/ember/appkit/rails/teaspoon/spec_controller.rb
+++ lib/ember/appkit/rails/teaspoon/spec_controller.rb
@@ -5,5 +5,3 @@ end
 ActiveSupport.on_load(:action_controller) do
   Teaspoon::SpecController.send(:prepend, Ember::Appkit::Rails::Teaspoon::SpecController) if defined?(Teaspoon::SpecController)
 end
-
-
