2c8db4184eae36961f1333458d3f5c9874ab7007
Aaron Renoir <aaron@fieldphone.com>
2014-03-14 14:26:53 -0700

    fix issue #201 eak_rails partial not in view path

--- lib/ember/appkit/rails/teaspoon.rb
+++ lib/ember/appkit/rails/teaspoon.rb
@@ -1,5 +1,6 @@
 module Ember::Appkit::Rails::Teaspoon; end
 
 require 'teaspoon'
+require 'ember/appkit/rails/teaspoon/engine'
 require 'ember/appkit/rails/teaspoon/suite'
 require 'ember/appkit/rails/teaspoon/spec_controller'
--- lib/ember/appkit/rails/teaspoon/spec_controller.rb
+++ lib/ember/appkit/rails/teaspoon/spec_controller.rb
@@ -1,13 +1,9 @@
 module Ember::Appkit::Rails::Teaspoon::SpecController
-  def self.prepended(mod)
-    mod.send(:before_filter, :extend_view_path)
-  end
-
-  def extend_view_path
-    append_view_path Ember::Appkit::Rails::Engine.root.join('lib', 'ember', 'appkit', 'rails', 'views')
-  end
+  
 end
 
 ActiveSupport.on_load(:action_controller) do
   Teaspoon::SpecController.send(:prepend, Ember::Appkit::Rails::Teaspoon::SpecController) if defined?(Teaspoon::SpecController)
 end
+
+
