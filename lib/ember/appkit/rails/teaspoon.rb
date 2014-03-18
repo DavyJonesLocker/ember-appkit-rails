9a516c2452d18b822384a8cdc4227ad1ea770d11
Aaron Renoir <aaron@fieldphone.com>
2014-03-17 17:48:01 -0700

    remove teaspoon spec_controller

--- lib/ember/appkit/rails/teaspoon.rb
+++ lib/ember/appkit/rails/teaspoon.rb
@@ -3,4 +3,3 @@ module Ember::Appkit::Rails::Teaspoon; end
 require 'teaspoon'
 require 'ember/appkit/rails/teaspoon/engine'
 require 'ember/appkit/rails/teaspoon/suite'
-require 'ember/appkit/rails/teaspoon/spec_controller'
--- lib/ember/appkit/rails/teaspoon/spec_controller.rb
+++
@@ -1,7 +0,0 @@
-module Ember::Appkit::Rails::Teaspoon::SpecController
-  
-end
-
-ActiveSupport.on_load(:action_controller) do
-  Teaspoon::SpecController.send(:prepend, Ember::Appkit::Rails::Teaspoon::SpecController) if defined?(Teaspoon::SpecController)
-end
